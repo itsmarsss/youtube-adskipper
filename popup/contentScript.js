af3f303e923cf97187f7ac24222a3431a8ca674d
itsmarsss <itsmarzzzzzz@gmail.com>
2023-11-04 23:43:25 -0400

    Add auto skipper

--- popup/contentScript.js
+++ popup/contentScript.js
@@ -5,13 +5,17 @@ const ascontent = document.createElement("as-content");
 
 const skipad = document.createElement("button");
 skipad.innerHTML = "Skip Ad";
+// skipad.addEventListener("click", () => {
+//     skipToEnd();
+// });
 
 ascontent.appendChild(skipad);
 
 const shadow = adskipper.attachShadow({ mode: "open" });
 const style = document.createElement("style");
 style.textContent = `
-#adskipper {
+as-content {
+    z-index: 69420;
     position: absolute;
     width: 20px;
     height: 30px;
@@ -23,9 +27,22 @@ shadow.appendChild(ascontent);
 
 document.body.appendChild(adskipper);
 
-function skipToEnd() {
-    var video = document.getElementsByTagName('video')[0];
-    var videoDuration = video.duration;
+setInterval(() => {
+    var skipbutton = document.getElementsByClassName('ytp-ad-skip-button');
+    var previewcont = document.getElementsByClassName('ytp-ad-preview-container');
 
-    video.currentTime = videoDuration;
-}
+    console.log(skipbutton.length + ":" + previewcont.length);
+
+    if (skipbutton.length != 0 || previewcont.length != 0) {
+        try {
+            var video = document.getElementsByTagName('video')[0];
+            video.currentTime = video.duration;
+        } catch (ex) {
+            console.log("Error skipping ad.");
+        }
+        if (skipbutton.length != 0) {
+            skipbutton[0].click();
+            console.log("Skip clicked.");
+        }
+    }
+}, 250);
