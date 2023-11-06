55f0cf8883a8a2e980b1886be07770c1ddbd7de9
itsmarsss <itsmarzzzzzz@gmail.com>
2023-11-05 23:35:29 -0500

    Remove logging

--- dev/src/contentScript.js
+++ dev/src/contentScript.js
@@ -15,11 +15,7 @@ const shadow = adskipper.attachShadow({ mode: "open" });
 const style = document.createElement("style");
 style.textContent = `
 as-content {
-    z-index: 69420;
-    position: absolute;
-    width: 20px;
-    height: 30px;
-    background: red;
+    // n/a
 }
 `;
 shadow.appendChild(style);
@@ -31,18 +27,18 @@ setInterval(() => {
     var skipbutton = document.getElementsByClassName('ytp-ad-skip-button');
     var previewcont = document.getElementsByClassName('ytp-ad-preview-container');
 
-    console.log(skipbutton.length + ":" + previewcont.length);
+    //console.log(skipbutton.length + ":" + previewcont.length);
 
     if (skipbutton.length != 0 || previewcont.length != 0) {
         try {
             var video = document.getElementsByTagName('video')[0];
             video.currentTime = video.duration;
         } catch (ex) {
-            console.log("Error skipping ad.");
+            //console.log("Error skipping ad.");
         }
         if (skipbutton.length != 0) {
             skipbutton[0].click();
-            console.log("Skip clicked.");
+            //console.log("Skip clicked.");
         }
     }
 }, 250);
--- out/src/contentScript.js
+++ out/src/contentScript.js
@@ -2,4 +2,4 @@ const adskipper = document.createElement("div"); adskipper.setAttribute("id", "a
 as-content {
     // n/a
 }
-`, shadow.appendChild(style), shadow.appendChild(ascontent), document.body.appendChild(adskipper), setInterval(() => { var e = document.getElementsByClassName("ytp-ad-skip-button"), t = document.getElementsByClassName("ytp-ad-preview-container"); if (console.log(e.length + ":" + t.length), 0 != e.length || 0 != t.length) { try { var a = document.getElementsByTagName("video")[0]; a.currentTime = a.duration } catch (n) { console.log("Error skipping ad.") } 0 != e.length && (e[0].click(), console.log("Skip clicked.")) } }, 250);
+`, shadow.appendChild(style), shadow.appendChild(ascontent), document.body.appendChild(adskipper), setInterval(() => { var e = document.getElementsByClassName("ytp-ad-skip-button"), t = document.getElementsByClassName("ytp-ad-preview-container"); if (0 != e.length || 0 != t.length) { try { var a = document.getElementsByTagName("video")[0]; a.currentTime = a.duration } catch (n) { } 0 != e.length && e[0].click() } }, 250);
