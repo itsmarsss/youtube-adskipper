0a4b5c96c2796867b2975910d71cc67318e979ae
itsmarsss <itsmarzzzzzz@gmail.com>
2023-11-12 02:01:33 -0500

    Refine UI

--- dev/src/popup.js
+++ dev/src/popup.js
@@ -1,8 +1,19 @@
-// let queryOptions = { active: true, lastFocusedWindow: true };
-// let [tab] = await chrome.tabs.query(queryOptions);
-// const { id, url } = tab;
-// chrome.scripting.executeScript(
-//     {
-//         target: { tabId: id, allFrames: true },
-//         files: ['/src/contentScript.js']
-//     });
+const toggle_button = document.getElementById('toggle-button');
+const toggle_button_icon = document.getElementById('toggle-button-icon');
+const toggle_text = document.getElementById('toggle-text');
+
+toggle_button.addEventListener('click', () => {
+    toggle_button_icon.classList.add('fade-out');
+    setTimeout(() => {
+        console.log(toggle_button_icon.innerHTML);
+        if (toggle_button_icon.innerHTML === 'pause_circle') {
+            toggle_button_icon.innerHTML = 'play_circle';
+            toggle_text.innerHTML = 'disabled';
+        } else {
+            toggle_button_icon.innerHTML = 'pause_circle';
+            toggle_text.innerHTML = 'enabled';
+        }
+        toggle_button_icon.classList.remove('fade-out');
+        toggle_text.classList.toggle('toggled-on');
+    }, 200);
+});
