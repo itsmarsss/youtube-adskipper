302235117bffcc2a00ab6858cb71948d9ac7bb95
itsmarsss <itsmarzzzzzz@gmail.com>
2023-11-21 19:22:18 -0500

    Check for button's parent insertion to DOM

--- dev/src/contentScript.js
+++ dev/src/contentScript.js
@@ -1,104 +1,81 @@
 var ytadskipper = (async function () {
-    const observerConfig = { attributes: true, childList: true, subtree: true };
 
-    let html5_video_container_node = null;
-    let observer = new MutationObserver(() => {
-        checkControllerNode();
-    });
+    function detectAddedElements() {
+        const targetNode = document.getElementsByClassName('html5-video-player')[0];
 
-    observer.observe(document, observerConfig);
 
-    function checkControllerNode() {
-        if (html5_video_container_node != null) {
-            observer.disconnect();
-            startSkipper();
-            return;
-        }
-        html5_video_container_node = document.getElementsByClassName('html5-video-container')[0];
-    }
-
-    function startSkipper() {
-        function skipSegment() {
-            function performCheck() {
-                function performSkip() {
-                    async function incrementSkipped() {
-                        chrome.storage.local.get(['skipped'], (result) => {
-                            chrome.storage.local.set({ skipped: (result.skipped || 0) + 1 });
-                        });
-                    }
-
-                    let skipbutton_old = document.getElementsByClassName('ytp-ad-skip-button');
-                    let previewcont_old = document.getElementsByClassName('ytp-ad-preview-container');
-                    let skip_button = document.querySelectorAll('[id ^= "skip-button:"]');
-
-                    if (skip_button.length >= 2) {
-                        skip_button[0].click();
-                        skip_button[1].click();
-                        incrementSkipped();
-                    } else if (skipbutton_old.length != 0 || previewcont_old.length != 0) {
-                        if (video.duration == NaN) {
-                            return;
+        const callback = function (mutationsList) {
+            for (let mutation of mutationsList) {
+                if (mutation.type === 'childList') {
+                    mutation.addedNodes.forEach(function (node) {
+                        if (node.nodeType === 1 && node.classList.contains('ytp-ad-player-overlay')) {
+                            skipSegment();
                         }
-
-                        video.currentTime = video.duration;
-
-                        if (skipbutton_old.length != 0) {
-                            skipbutton_old[0].click();
-                        }
-                        incrementSkipped();
-                    }
-                }
-
-                let video = document.getElementsByTagName('video');
-
-                if (video.length == 0) {
-                    return;
-                }
-
-                video = video[0];
-
-                if (video.readyState >= 2) {
-                    performSkip();
-                } else {
-                    video.addEventListener('loadeddata', () => {
-                        performSkip();
                     });
                 }
             }
+        };
 
-            chrome.storage.local.get(['enabled'], (result) => {
-                if (result.enabled || false) {
-                    performCheck();
-                }
-            });
-        }
+        const observer = new MutationObserver(callback);
 
-        skipSegment();
+        observer.observe(targetNode, { attributes: true, childList: true, subtree: true });
+    }
 
-        let observer = new MutationObserver((mutations) => {
-            function isVideo(nodeList) {
-                for (let i = 0; i < nodeList.length; i++) {
-                    if (nodeList[i]['className'] == 'video-stream html5-main-video') {
-                        return true;
-                    }
-                };
-                return false;
+    detectAddedElements();
+
+    function skipSegment() {
+        chrome.storage.local.get(['enabled'], (result) => {
+            if (result.enabled || false) {
+                console.log(result.enabled)
+                waitForVideoLoad();
             }
-
-            mutations.forEach((mutation) => {
-                if (mutation.type == 'childList') {
-                    if (isVideo(mutation.addedNodes) || isVideo(mutation.removedNodes)) {
-                        skipSegment();
-                    }
-                } else if (mutation.type == 'attributes') {
-                    if (mutation.attributeName == 'src') {
-                        skipSegment();
-                    }
-                }
-            });
         });
 
-        observer.observe(html5_video_container_node, observerConfig);
+        function waitForVideoLoad() {
+            let video = document.getElementsByTagName('video');
+
+            if (video.length == 0) {
+                return;
+            }
+
+            video = video[0];
+
+            if (video.readyState >= 2) {
+                performSkip();
+            } else {
+                video.addEventListener('loadeddata', () => {
+                    performSkip();
+                });
+            }
+
+            function performSkip() {
+                async function incrementSkipped() {
+                    chrome.storage.local.get(['skipped'], (result) => {
+                        chrome.storage.local.set({ skipped: (result.skipped || 0) + 1 });
+                    });
+                }
+
+                let skipbutton_old = document.getElementsByClassName('ytp-ad-skip-button');
+                let previewcont_old = document.getElementsByClassName('ytp-ad-preview-container');
+                let skipbutton_new = document.getElementsByClassName('ytp-ad-skip-button-modern');
+
+                if (skipbutton_new.length != 0) {
+                    skipbutton_new[0].click();
+                    incrementSkipped();
+                } else if (skipbutton_old.length != 0 || previewcont_old.length != 0) {
+                    if (video.duration == NaN) {
+                        return;
+                    }
+
+                    video.currentTime = video.duration;
+
+                    if (skipbutton_old.length != 0) {
+                        skipbutton_old[0].click();
+                    }
+                    incrementSkipped();
+                }
+            }
+        }
     }
 });
 
